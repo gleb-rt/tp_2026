24748b5df23d1fce03be7cb81189062d2a9fd911
Alexei Tchervinsky <chervinskij_ap@spbstu.ru>
2026-04-15 23:19:49 +0300

    v5 rebranch, repush

---
+++ tchervinsky.alexei/T4/Shape.hpp
@@ -0,0 +1,19 @@
+#ifndef SHAPE_HPP
+#define SHAPE_HPP
+#include "Point.hpp"
+#include "FrameRectangle.hpp"
+#include <string>
+
+class Shape
+{
+public:
+	virtual ~Shape() {}
+	virtual double getArea() = 0;
+	virtual FrameRectangle getFrameRectangle() = 0;
+	virtual void move(const double k) = 0;
+	virtual void move(const double x, const double y) = 0;
+	virtual void scale(const double k) = 0;
+	virtual Shape *clone() = 0;
+	virtual std::string getName() const = 0;
+};
+#endif
