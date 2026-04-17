8a771eea50c546877d87f1d04a0afe990f95c8de
a-sik <andreyskor07@gmail.com>
2026-04-17 03:22:40 +0300

    Added the declaration and definition Circle

---
+++ skorohod.andrey/T4/circle.hpp
@@ -0,0 +1,24 @@
+#ifndef CIRCLE_HPP
+#define CIRCLE_HPP
+
+#include "point.hpp"
+#include "shape.hpp"
+
+class Circle : public Shape
+{
+    public:
+    Circle(Point, double);
+    ~Circle() = default;
+
+    double getArea() const; 
+    Point getCenter() const;
+    void move(double, double);
+    void scale(double);
+    std::string getName() const;
+
+    private:
+    Point center_;
+    double radius_;
+};
+
+#endif
